72d0b6f4c36a1c077076b1f08196fe0752de3a8e
elguesabal <joseelguesabal@gmail.com>
2025-10-08 19:38:05 -0300

    criando repositorio para estudar a lib expo-notifications

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { useState, useEffect } from "react";
+import * as Notifications from 'expo-notifications';
+
+Notifications.setNotificationHandler({
+	handleNotification: async () => ({
+		// shouldShowAlert: true, // OBSOLETO
+		shouldShowBanner: true,
+		// shouldShowList: true,
+		shouldPlaySound: true,
+		shouldSetBadge: true
+	})
+});
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [token, setToken] = useState("");
+
+	useEffect(() => {
+		// async function getToken() {
+		// 	const settings = await Notifications.getPermissionsAsync();
+		// 	if (!settings.granted) await Notifications.requestPermissionsAsync();
+		// 	setToken((await Notifications.getExpoPushTokenAsync()).data);
+		// }
+		// getToken();
+
+		async function notification() {
+			await Notifications.scheduleNotificationAsync({
+				content: {
+					title: "title",
+					body: "body",
+				},
+				trigger: {
+				}
+			});
+		}
+		notification();
+	}, []);
+// console.log("token: ", token)
+	return (
+		<View style={styles.container} >
+			<Text>{token}</Text>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
